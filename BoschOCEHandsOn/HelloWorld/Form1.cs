67565cccddcdc3b0a829c41a91b6c16086317371
rdoe <r.doering@osb-ag.de>
2017-05-12 09:34:35 +0200

    created just a small HelloWorld Application

---
+++ BoschOCEHandsOn/HelloWorld/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using commlpiLib;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+
+        MlpiConnection connection = new MlpiConnection();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connection.Connect("192.168.1.103 -user=labview -password=labview2015");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (connection.IsConnected)
+            {
+                //var diag = connection.System.GetDisplayedDiagnosis().text;
+                //label1.Text = diag; 
+
+                //var axes = connection.Motion.GetConfiguredAxes();
+                connection.System.SetTargetMode(SystemMode.SYSTEMMODE_BB);
+                AxisInformation[] axisInfo = (AxisInformation[])connection.Motion.GetConfiguredAxes();
+
+                foreach (var ax in axisInfo)
+                {
+                    
+                    Console.WriteLine(ax.);
+                }
+
+            } 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            connection.Disconnect();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # BoschOCEHandsOn
 This Repo holds the code developed during a hands on session with Bosch control hardware using the Open Core Interface
+
+
+### General Facts
+
+- user management auf der Steuerung
+- standardmäßig nur Leserechte
+- Schreibrechte mit entsprechendem Username und Passwort (labview, labview2015)
+- Rechteverwaltung auf der Steuerung in entsprechnder XML-Datei
+
+### C# Stuff
+
+- `MlpiConnection` object to establish connection
+- `Motion` object to control axes
